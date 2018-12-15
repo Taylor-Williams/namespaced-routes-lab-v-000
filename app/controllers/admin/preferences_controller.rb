f46a2df92a45ba9b60be9e0563a0d5274939553a
Taylor Williams <taylor@thewilliams.net>
2018-12-15 23:41:48 +0000

    Automatically backed up by Learn

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -1,9 +1,6 @@
 class Admin::PreferencesController < ApplicationController
 
   def index
-    if Preference.all.empty?
-      Preference.create(artist_sort_order: "DESC", song_sort_order: "ASC", allow_create_artists: false, allow_create_songs: true)
-    end
     @preference = Preference.first
   end
 
